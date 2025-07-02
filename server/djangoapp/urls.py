7ac69e03f935a3dca2c0df1642568a387faa3529
nidhukrishna <nidhukrishnag@gmail.com>
2025-07-01 21:12:50 -0400

    module 2.2

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     # path for login
     path(route='login', view=views.login_user, name='login'),
     path('logout', views.logout_user, name='logout'),
-
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
     # path for dealer reviews view
 
     # path for add a review view
